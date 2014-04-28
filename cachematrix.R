9a99f8e8d6359a9549d10bc3eec6ce3d032f25d8
smithlsh <smithlsh@users.noreply.github.com>
2014-04-27 21:55:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 # inverse.  Usage: cacheMatrix<-MakeCacheMatrix(yourMatrix).  You should not directly
 # call any of the Set routines.  The cacheMatrix is the argument to CacheSolve.
 #
-MakeCacheMatrix <- function(myCacheMat=NULL) {
+makeCacheMatrix <- function(myCacheMat=NULL) {
 #
 # Some Rudimentary Error Checking.  It is not perfect!!!
 #
@@ -41,45 +41,18 @@ MakeCacheMatrix <- function(myCacheMat=NULL) {
 }
 
 
-
 #
-# This function creates the CacheMatrix, a special matrix that cashes its
-# inverse.  Usage: cacheMatrix<-MakeCacheMatrix(yourMatrix).  You should not directly
-# call any of the Set routines.  The cacheMatrix is the argument to CacheSolve.
+# This function should be used to compute the inverse of the special
+# Cachematrix. It take a special CacheMatrix as its argument.
 #
-MakeCacheMatrix <- function(myCacheMat=NULL) {
-#
-# Some Rudimentary Error Checking.  It is not perfect!!!
-#
-        retNullMsg1<-"The CacheMatrix your are attempting to make is empty, null"
-        retNullMsg2<-"or does not exist. Try again."
-        retSqMsg1<-"The CacheMatrix you are making is not square or has "
-        retSqMsg2<-"a negative number of rows or columns."
-        if (is.na(myCacheMat) || is.null(myCacheMat)) {
-          cat(retNullMsg1)
-          cat(retNullMsg2)
-          return
+cacheSolve <- function(x, ...) {
+        myCacheMatInverse <- x$GetCacheMatInverse()
+        if(!is.null(myCacheMatInverse)) {
+                message("getting cached data")
+                return(myCacheMatInverse)
         }
-
-        numCols=ncol(myCacheMat)
-        numRows=nrow(myCacheMat)
-         if ((numRows != numCols) || numRows<=0 || numCols<=0) {
-           cat(retSqMsg1)
-           cat(retSqMsg2)
-           return
-         }
-#
-        myCacheMatInverse <- NULL
-        SetCacheMat <- function(anotherCacheMat) {
-             myCacheMat <<- anotherMat
-             myCacheMatInverse <<- NULL
-        }
-        GetCacheMat <- function() myCacheMat
-
-        SetCacheMatInverse <- function(matInverse) myCacheMatInverse <<-matInverse
-
-        GetCacheMatInverse <- function() myCacheMatInverse
-
-        list(SetCacheMat=SetCacheMat,GetCacheMat=GetCacheMat,
-        SetCacheMatInverse=SetCacheMatInverse,GetCacheMatInverse=GetCacheMatInverse)
+        mat <- x$GetCacheMat()
+        myCacheMatInverse <-solve(mat)
+        x$SetCacheMatInverse(myCacheMatInverse)
+        myCacheMatInverse
 }
